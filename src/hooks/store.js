e14220bfbad04196f05a4256aa69f13226945caf
zamao9 <mansory_09@mail.ru>
2025-01-17 20:09:34 +0300

    Updates

--- src/hooks/store.js
+++
@@ -1,5 +0,0 @@
-// src/hooks/store.js
-import { useDispatch, useSelector } from 'react-redux';
-
-export const useAppDispatch = () => useDispatch();
-export const useAppSelector = (selector) => useSelector(selector);
